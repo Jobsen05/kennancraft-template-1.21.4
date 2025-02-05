30bb268d53a6e31ee3d7474f459fffd3f74d8145
Jonatan <jonatanjobs@gmail.com>
2025-02-05 23:18:16 +0100

    Very Important

---
+++ src/main/java/net/jobsen/kennancraft/KennanCraftClient.java
@@ -0,0 +1,10 @@
+package net.jobsen.kennancraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KennanCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
